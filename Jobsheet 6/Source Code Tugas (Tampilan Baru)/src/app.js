5c8283a2a5d9c5d4132ba9f65e340f6fb9f1a5f1
SirGhazian <ghazianunp99@gmail.com>
2025-10-24 22:35:30 +0700

    add jobsheet 5

---
+++ Jobsheet 6/Source Code Tugas (Tampilan Baru)/src/app.js
@@ -0,0 +1,94 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/prediksiCuaca");
+
+const direktoriPublic = path.join(__dirname, "../public");
+const direktoriViews = path.join(__dirname, "../templates/views");
+const direktoriPartials = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", direktoriViews);
+hbs.registerPartials(direktoriPartials);
+
+app.use(express.static(direktoriPublic));
+
+// Halaman Utama
+app.get("", (req, res) => {
+  res.render("index", {
+    judul: "Aplikasi Cek Cuaca",
+    nama: "Muhammad Ghazian Tza",
+    ikon: "/img/cuacaIcon.png",
+  });
+});
+
+// Halaman Tentang
+app.get("/tentang", (req, res) => {
+  res.render("tentang", {
+    judul: "Halaman Tentang",
+    nama: "Muhammad Ghazian Tza",
+    ikon: "/img/cuacaIcon.png",
+  });
+});
+
+// Halaman Bantuan
+app.get("/bantuan", (req, res) => {
+  res.render("bantuan", {
+    judul: "Halaman Bantuan",
+    teksBantuan: "Ini adalah teks bantuan",
+    nama: "Muhammad Ghazian Tza",
+    ikon: "/img/cuacaIcon.png",
+  });
+});
+
+// Halaman Info Cuaca
+app.get("/infocuaca", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "[ Kamu harus memasukan lokasi yang ingin dicari! ]",
+    });
+  }
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, dataPrediksi) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        prediksiCuaca: dataPrediksi,
+        lokasi: location,
+        address: req.query.address,
+      });
+    });
+  });
+});
+
+// Halaman 404 untuk /bantuan/*
+app.get(/^\/bantuan\/.*/, (req, res) => {
+  res.render("404", {
+    judul: "404",
+    nama: "Muhammad Ghazian Tza",
+    pesanKesalahan: "Artikel yang dicari tidak ditemukan.",
+    ikon: "/img/cuaca.png",
+  });
+});
+
+// Halaman 404 umum untuk semua rute lain
+app.get(/.*/, (req, res) => {
+  res.render("404", {
+    judul: "404",
+    nama: "Muhammad Ghazian Tza",
+    pesanKesalahan: "Halaman tidak ditemukan.",
+    ikon: "/img/cuaca.png",
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Server berjalan di http://localhost:4000");
+});
---
+++ Jobsheet 6/Source Code/public/js/app.js
@@ -0,0 +1,26 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const pesanSatu = document.querySelector("#pesan-1");
+const pesanDua = document.querySelector("#pesan-2");
+
+// pesanSatu.textContent = 'From JavaScript'
+
+weatherForm.addEventListener("submit", (e) => {
+  console.log("Form disubmit");
+  e.preventDefault();
+
+  const location = search.value;
+  pesanSatu.textContent = "Sedang mencari lokasi...";
+  pesanDua.textContent = "";
+
+  fetch("http://localhost:4000/infocuaca?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        pesanSatu.textContent = data.error;
+      } else {
+        pesanSatu.textContent = data.lokasi;
+        pesanDua.textContent = data.prediksiCuaca;
+      }
+    });
+  });
+});
